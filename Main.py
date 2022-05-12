426b478df42981e073f2403a6f02281b151a8244
heso6 <heso67@gmail.com>
2022-05-13 00:49:48 +0500

    telegram error notifications

--- Main.py
+++ Main.py
@@ -1,18 +1,32 @@
+import os
+
 from loguru import logger
+from notifiers.logging import NotificationHandler
+from dotenv import load_dotenv
 
 import Houses
 import Deals
 import Leads
 
+load_dotenv()
+
 
 @logger.catch()
 def main():
+    defaults = {
+        'token': os.environ.get('telegram_token'),
+        'chat_id':  os.environ.get('chat_id')
+    }
+    handler = NotificationHandler('telegram', defaults=defaults)
+
     logger.add('logs/log.json',
                format='{time:YYYY-MM-DD HH:mm:ss} {level} {message}',
                level='INFO',
                rotation='10 MB',
                retention='30 days',
                serialize=True)
+    logger.add(handler, level="ERROR")
+
     Houses.main()
     Deals.main()
     Leads.main()
